b227c112b6f1baaa01c2fab1ec9abef288b2307c
Paul Blizniuk <paulblizniuk@gmail.com>
2024-04-26 13:55:54 -0400

    fix about logo on mobile

--- src/app/_widgets/who_we_are/index.tsx
+++ src/app/_widgets/who_we_are/index.tsx
@@ -16,19 +16,19 @@ const WhoWeAreWidget = (props: Props) => {
         description={description}
       />
       <div className='relative m-auto w-full max-w-[1340px] flex p-8 text-center md:p-12 md:text-right lg:my-24 lg:px-16'>
-        <div className='about-logo relative flex items-center mx-auto'>
+        <div className='about-logo relative flex flex-col md:flex-row items-center mx-auto'>
           <div className="relative">
             <Image
               src='/assets/logo-xl-new.png'
               alt='logo'
               width={250}
               height={250}
-              className='transition-all duration-2000 grayscale group-hover:grayscale-0 relative z-20'
+              className='transition-all duration-2000 grayscale group-hover:grayscale-0 relative z-20 mb-6 md:mb-0'
             />
             <div className='animate-pulse absolute end-0 top-0 z-10 h-[250px] w-[250px] rounded-full bg-stone-800 object-contain opacity-40 blur-3xl'></div>
           </div>
           <span
-            className={`pl-3 font-rock-salt text-6xl font-extrabold transition-all text-black`}
+            className={`pl-3 font-rock-salt text-4xl md:text-6xl font-extrabold transition-all text-black`}
           >
             BitEnvious
           </span>
