e4e737167f05b93c0549aaa35499ebb95c8ad265
Paul Blizniuk <paulblizniuk@gmail.com>
2024-05-21 12:18:07 -0400

    turn off caching on pills

--- src/app/_components/home_page_hero/index.tsx
+++ src/app/_components/home_page_hero/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
-
+export const revalidate = 0
 type Props = {
   Title: string
   Description: string
